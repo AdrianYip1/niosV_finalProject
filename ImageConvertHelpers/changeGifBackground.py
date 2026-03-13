d575909a68cc8ffb0505fa7bc5797733852404d7
AdrianYip1 <yipadrian1119@gmail.com>
2026-03-13 10:09:03 -0400

    fix: changeGifToBackground.py detects transparent

--- ImageConvertHelpers/changeGifBackground.py
+++ ImageConvertHelpers/changeGifBackground.py
@@ -1,32 +1,38 @@
 from PIL import Image
 
-OLD_BG = (0, 0, 0)       # current background (RGB)
-NEW_BG = (255, 0, 255)   # new background (transparent)
+PINK = (255, 0, 255, 255)  # transparent colour (pink)
 
-im = Image.open("anim.gif")
+im = Image.open("in.gif")
 frames = []
+frame = 0
 
-frame_idx = 0
 while True:
     try:
-        im.seek(frame_idx)
+        im.seek(frame)
     except EOFError:
         break
 
-    frame = im.convert("RGB")
-    pixels = frame.load()
-    w, h = frame.size
+    f = im.convert("RGBA")
+    pixels = f.load()
+    w, h = f.size
+
     for y in range(h):
         for x in range(w):
-            if pixels[x, y] == OLD_BG:
-                pixels[x, y] = NEW_BG
-    frames.append(frame)
-    frame_idx += 1
+            r, g, b, a = pixels[x, y]
+            if a == 0:           # transparent pixel
+                pixels[x, y] = PINK
 
+
+    frames.append(f)
+    frame += 1
+
+# save back to a new GIF
 frames[0].save(
-    "anim_bg_changed.gif",
+    "out_pink_bg.gif",
     save_all=True,
     append_images=frames[1:],
     loop=0,
-    duration=im.info.get("duration", 100)
+    duration=im.info.get("duration", 100),
+    transparency=None,
+    disposal=2,
 )
