4035809a1cc97b86d1052fdd34810992a34dec12
AdrianYip1 <yipadrian1119@gmail.com>
2026-03-19 23:15:15 -0400

    added battling engine with pokemon attacks, types, database of stats

--- software/gameplayLogic/entities/pokemonObject.c
+++ software/gameplayLogic/entities/pokemonObject.c
@@ -1,86 +1,76 @@
 #include "pokemonObject.h"
 
-typedef struct{
-    int monster_ID;
+void initPokemon(PokemonData* pokemon, int id, const char* name,
+                 int baseHp, int baseAttack, int baseSpAttack,
+                 int baseDefense, int baseSpDefense, int baseSpeed,
+                 PokemonType type1, PokemonType type2,
+                 const AttackData* move1, const AttackData* move2,
+                 const AttackData* move3, const AttackData* move4) {
 
-    int frontFrame_ID;
-    int backFrame_ID;
+    pokemon->id = id;
+    pokemon->name = name;
+    pokemon->baseHp = baseHp;
+    pokemon->baseAttack = baseAttack;
+    pokemon->baseSpAttack = baseSpAttack;
+    pokemon->baseDefense = baseDefense;
+    pokemon->baseSpDefense = baseSpDefense;
+    pokemon->baseSpeed = baseSpeed;
 
-    int* baseStats[5]; //{hp, attack, sp attack, def, sp def, speed} array from a monster template
-} pokemonID;
+    pokemon->type1 = type1;
+    pokemon->type2 = type2;
 
-typedef struct{
-    pokemonID id;
-    int level;
-    int exp;
-    bool alive;
+    pokemon->moves[0] = move1;
+    pokemon->moves[1] = move2;
+    pokemon->moves[2] = move3;
+    pokemon->moves[3] = move4;
+}
 
-    int* scaledStatsWithLevel[5]; //{hp, attack, sp attack, def, sp def, speed} array from a monster template
+void initPokemonInBattle(pokemonInBattle* pokemon, const PokemonData* template, int level) {
+    pokemon->id.data = template;
+    pokemon->id.frontFrame_ID = template->id;
+    pokemon->id.backFrame_ID = template->id;
 
-    bool inBattle;
-    char* attacks[3]; // 4 attacks per pokemon
-} pokemonInBattle;
+    pokemon->level = level;
+    pokemon->exp = 0;
+    pokemon->alive = true;
+    pokemon->inBattle = false;
 
-void scaleStatsWithLevel(pokemonInBattle pokemon) {
-    for (int i = 0; i < 5; i++) {
-        pokemon.scaledStatsWithLevel[i] = (((int)pokemon.id.baseStats[i] * 2) / 100 * pokemon.level);
+    pokemon->status = STATUS_NONE;
+    pokemon->sleepTurnsRemaining = 0;
+
+    pokemon->type1 = template->type1;
+    pokemon->type2 = template->type2;
+
+    for (int i = 0; i < 4; i++) {
+        pokemon->attacks[i] = template->moves[i];
+        pokemon->currentPP[i] = (template->moves[i] != NULL) ? template->moves[i]->maxPP : 0;
+    }
+    scaleStatsWithLevel(pokemon);
+    pokemon->maxHp = pokemon->scaledStatsWithLevel[0];
+}
+
+void scaleStatsWithLevel(pokemonInBattle* pokemon) {
+    const PokemonData* d = pokemon->id.data;
+    int baseStats[6] = {d->baseHp, d->baseAttack, d->baseSpAttack, d->baseDefense, d->baseSpDefense, d->baseSpeed};
+    for (int i = 0; i < 6; i++) {
+        pokemon->scaledStatsWithLevel[i] = (((int)baseStats[i] * 2) / 100 * pokemon->level);
     }
 }
 
-void gainExp(pokemonInBattle pokemon, pokemonInBattle defeatedPokemon) {
-    int expGained = experienceGained(pokemon.level, defeatedPokemon.level);
-
+void gainExp(pokemonInBattle* pokemon, pokemonInBattle* defeatedPokemon) {
+    int expGained = experienceGained(pokemon->level, defeatedPokemon->level);
     if (expGained == 0) return;
-
-    while (experienceGained > 0) {
-        pokemon.exp += expGained;
-        if (pokemon.exp > expRequiredAtLevel(pokemon.level)) {
-            levelUp(pokemon);
-        }
+    pokemon->exp += expGained;
+    while (pokemon->exp >= expRequiredAtLevel(pokemon->level)) {
+        levelUp(pokemon);
     }
 }
 
-void dealDamage(pokemonInBattle attacker, pokemonInBattle target, int baseDamage, int damageType) { //0 is physical, 1 is special, 2 is status
-    int modifierID = (damageType == 1) ? 1 : 2; //physical uses attack, special uses sp attack
-    int dealtDamage = ((baseDamage * attacker.level + 10) / 250) * (int)attacker.scaledStatsWithLevel[modifierID];
-
-    takeDamage(target, dealDamage, damageType);
-
-    return;
-}
-
-void takeDamage(pokemonInBattle target, int incomingDamage, int damageType) {
-    srand(time(NULL));
-
-    double min = 0.85;
-    double max = 1.0;
-    double random_num;
-
-    // Generate a random double between 0.0 and 1.0 using the standard method
-    double scale = (double)rand() / (double)RAND_MAX;
-
-    random_num = min + scale * (max - min);
-
-    //calculate damage with basestats
-    int modifierID = (damageType == 1) ? 3 : 4; // physicaluses def, "Special" uses spdef
-    int actualDamage = (incomingDamage / (int)target.scaledStatsWithLevel[modifierID]) * random_num; //damage calculations
-
-    if (actualDamage > target.scaledStatsWithLevel[0]) {
-        target.alive = false; //kill pokemon 
-    }
-    else {
-        target.scaledStatsWithLevel[0] -= actualDamage;
-    }
-
-    return;
-}
-
-void levelUp(pokemonInBattle pokemon) {
-    int currentExp = pokemon.exp;
-    int neededExp = expRequiredAtLevel(pokemon.level);
-
-    currentExp -= neededExp;
-    pokemon.level++;
+void levelUp(pokemonInBattle* pokemon) {
+    pokemon->exp -= expRequiredAtLevel(pokemon->level);
+    pokemon->level++;
+    scaleStatsWithLevel(pokemon);
+    pokemon->maxHp = pokemon->scaledStatsWithLevel[0];
 }
 
 int expRequiredAtLevel(int level) {
@@ -91,30 +81,173 @@ int experienceGained(int levelSelf, int levelOpponent) {
     return ((BASE_EXP_GAINED * levelOpponent) / 7) * EXP_MULTIPLIER;
 }
 
-bool isAlive(pokemonInBattle pokemon) {
-    return pokemon.alive;
+float getTypeEffectiveness(PokemonType attackType, PokemonType defendType1, PokemonType defendType2) {
+    float effectiveness = typeChart[attackType][defendType1];
+    if (defendType2 != defendType1) {
+        effectiveness *= typeChart[attackType][defendType2];
+    }
+    return effectiveness;
 }
 
-int getHp(pokemonInBattle pokemon) {
-    return pokemon.id.baseStats[0];
-}
-int getAttack(pokemonInBattle pokemon){
-    return pokemon.id.baseStats[1];
-}
-
-int getSpAttack(pokemonInBattle pokemon){
-    return pokemon.id.baseStats[2];
+void dealDamage(pokemonInBattle* attacker, pokemonInBattle* target, int baseDamage, int damageType, PokemonType moveType) { //0 is physical, 1 is special, 2 is status
+    int modifierID = (damageType == 0) ? 1 : 2; //physical uses attack, special uses sp attack
+    int dealtDamage = ((baseDamage * attacker->level + 10) / 250) * attacker->scaledStatsWithLevel[modifierID];
+    float effectiveness = getTypeEffectiveness(moveType, target->type1, target->type2);
+    dealtDamage = (int)(dealtDamage * effectiveness);
+    takeDamage(target, dealtDamage, damageType);
+    return;
 }
 
-int getDef(pokemonInBattle pokemon) {
-    return pokemon.id.baseStats[3];
+void takeDamage(pokemonInBattle* target, int incomingDamage, int damageType) {
+    double min = 0.85;
+    double max = 1.0;
+    double random_num;
+    // Generate a random double between 0.0 and 1.0 using the standard method
+    double scale = (double)rand() / (double)RAND_MAX;
+    random_num = min + scale * (max - min);
+    int modifierID = (damageType == 0) ? 3 : 4; // physical uses def, special uses spdef
+    int actualDamage = (int)((double)incomingDamage / target->scaledStatsWithLevel[modifierID] * random_num); //damage calculations
+    if (actualDamage > target->scaledStatsWithLevel[0]) {
+        target->alive = false; //kill pokemon
+        target->scaledStatsWithLevel[0] = 0;
+    }
+    else {
+        target->scaledStatsWithLevel[0] -= actualDamage;
+    }
+    return;
 }
 
-int getSpDef(pokemonInBattle pokemon) {
-    return pokemon.id.baseStats[4];
+bool useAttack(pokemonInBattle* attacker, pokemonInBattle* target, int attackIndex) {
+    if (attackIndex < 0 || attackIndex > 3) return false;
+    if (attacker->attacks[attackIndex] == NULL) return false;
+
+    const AttackData* move = attacker->attacks[attackIndex];
+    if (attacker->currentPP[attackIndex] <= 0) return false;
+
+    int accuracyRoll = rand() % 100;
+    if (accuracyRoll >= move->accuracy) return false;
+
+    attacker->currentPP[attackIndex]--;
+    if (move->category != ATTACK_STATUS) {
+        dealDamage(attacker, target, move->power, move->category, move->type);
+    }
+    return true;
 }
 
-int getSpd(pokemonInBattle pokemon) {
-    return pokemon.id.baseStats[5];
+int determineturnOrder(pokemonInBattle* pokemon1, pokemonInBattle* pokemon2) { //returns 1 if pokemon1 goes first, 2 if pokemon2 goes first
+    int spd1 = pokemon1->scaledStatsWithLevel[5];
+    int spd2 = pokemon2->scaledStatsWithLevel[5];
+    if (spd1 > spd2) return 1;
+    if (spd2 > spd1) return 2;
+    return (rand() % 2 == 0) ? 1 : 2; //speed tie broken randomly
 }
 
+bool attemptFlee(pokemonInBattle* fleeing, pokemonInBattle* opponent) {
+    int fleeingSpd = fleeing->scaledStatsWithLevel[5];
+    int opponentSpd = opponent->scaledStatsWithLevel[5];
+    if (fleeingSpd >= opponentSpd) return true;
+    int fleeChance = (fleeingSpd * 128 / opponentSpd) + 30;
+    return (rand() % 256) < fleeChance;
+}
+
+void applyStatusEffect(pokemonInBattle* pokemon, StatusCondition status) {
+    if (pokemon->status != STATUS_NONE) return;
+    pokemon->status = status;
+    if (status == STATUS_SLEEP) {
+        pokemon->sleepTurnsRemaining = (rand() % 4) + 1;
+    }
+}
+
+void tickStatusEffect(pokemonInBattle* pokemon) {
+    switch (pokemon->status) {
+        case STATUS_BURN:
+            takeDamage(pokemon, pokemon->maxHp / 8, ATTACK_STATUS); //burn deals 1/8 max hp per turn
+            break;
+        case STATUS_POISON:
+            takeDamage(pokemon, pokemon->maxHp / 16, ATTACK_STATUS); //poison deals 1/16 max hp per turn
+            break;
+        case STATUS_SLEEP:
+            if (pokemon->sleepTurnsRemaining > 0) {
+                pokemon->sleepTurnsRemaining--;
+            }
+            if (pokemon->sleepTurnsRemaining == 0) {
+                pokemon->status = STATUS_NONE;
+            }
+            break;
+        case STATUS_FREEZE:
+            if (rand() % 100 < 20) { //20% chance to thaw each turn
+                pokemon->status = STATUS_NONE;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+bool canAct(pokemonInBattle* pokemon) {
+    if (!pokemon->alive) return false;
+    if (pokemon->status == STATUS_SLEEP) return false;
+    if (pokemon->status == STATUS_FREEZE) return false;
+    if (pokemon->status == STATUS_PARALYSIS) {
+        if (rand() % 100 < 25) return false; //25% chance to be fully paralyzed
+    }
+    return true;
+}
+
+void healPokemon(pokemonInBattle* pokemon, int amount) {
+    pokemon->scaledStatsWithLevel[0] += amount;
+    if (pokemon->scaledStatsWithLevel[0] > pokemon->maxHp) {
+        pokemon->scaledStatsWithLevel[0] = pokemon->maxHp;
+    }
+}
+
+void restorePP(pokemonInBattle* pokemon, int attackIndex) {
+    if (attackIndex < 0 || attackIndex > 3) return;
+    if (pokemon->attacks[attackIndex] == NULL) return;
+    pokemon->currentPP[attackIndex] = pokemon->attacks[attackIndex]->maxPP;
+}
+
+void restoreAllPP(pokemonInBattle* pokemon) {
+    for (int i = 0; i < 4; i++) {
+        restorePP(pokemon, i);
+    }
+}
+
+void fullHeal(pokemonInBattle* pokemon) {
+    healPokemon(pokemon, pokemon->maxHp);
+    restoreAllPP(pokemon);
+    pokemon->status = STATUS_NONE;
+    pokemon->sleepTurnsRemaining = 0;
+}
+
+bool attemptCatch(pokemonInBattle* wildPokemon, float ballModifier) {
+    float hpFactor = 1.0f + 2.0f * (1.0f - (float)wildPokemon->scaledStatsWithLevel[0] / wildPokemon->maxHp);
+    float statusBonus = 1.0f;
+    if (wildPokemon->status == STATUS_SLEEP || wildPokemon->status == STATUS_FREEZE) statusBonus = 2.0f;
+    if (wildPokemon->status == STATUS_PARALYSIS || wildPokemon->status == STATUS_BURN || wildPokemon->status == STATUS_POISON) statusBonus = 1.5f;
+    float catchChance = hpFactor * statusBonus * ballModifier;
+    int catchRoll = rand() % 256;
+    return catchRoll < (int)(catchChance * 50);
+}
+
+bool isAlive(pokemonInBattle* pokemon) {
+    return pokemon->alive;
+}
+int getHp(pokemonInBattle* pokemon) {
+    return pokemon->id.data->baseHp;
+}
+int getAttack(pokemonInBattle* pokemon){
+    return pokemon->id.data->baseAttack;
+}
+int getSpAttack(pokemonInBattle* pokemon){
+    return pokemon->id.data->baseSpAttack;
+}
+int getDef(pokemonInBattle* pokemon) {
+    return pokemon->id.data->baseDefense;
+}
+int getSpDef(pokemonInBattle* pokemon) {
+    return pokemon->id.data->baseSpDefense;
+}
+int getSpd(pokemonInBattle* pokemon) {
+    return pokemon->id.data->baseSpeed;
+}
--- software/gameplayLogic/entities/pokemonObject.h
+++ software/gameplayLogic/entities/pokemonObject.h
@@ -1,6 +1,193 @@
+#pragma once
 #include <stdbool.h>
+#include <stddef.h>
+#include <math.h>
 #include <stdlib.h>
 #include <time.h>
-#include "pokemonDataBase.h"
-#define BASE_EXP_GAINED 15
-#define EXP_MULTIPLIER 1.5 //for balancing the game
+
+#define BASE_EXP_GAINED 50
+#define EXP_MULTIPLIER 1
+#define NUM_TYPES 18
+
+typedef enum {
+    TYPE_NORMAL = 0,
+    TYPE_FIRE,
+    TYPE_WATER,
+    TYPE_ELECTRIC,
+    TYPE_GRASS,
+    TYPE_ICE,
+    TYPE_FIGHTING,
+    TYPE_POISON,
+    TYPE_GROUND,
+    TYPE_FLYING,
+    TYPE_PSYCHIC,
+    TYPE_BUG,
+    TYPE_ROCK,
+    TYPE_GHOST,
+    TYPE_DRAGON,
+    TYPE_DARK,
+    TYPE_STEEL,
+    TYPE_FAIRY
+} PokemonType;
+
+typedef enum {
+    ATTACK_PHYSICAL = 0,
+    ATTACK_SPECIAL = 1,
+    ATTACK_STATUS = 2
+} AttackCategory;
+
+typedef enum {
+    STATUS_NONE = 0,
+    STATUS_BURN,
+    STATUS_POISON,
+    STATUS_PARALYSIS,
+    STATUS_SLEEP,
+    STATUS_FREEZE
+} StatusCondition;
+
+typedef struct {
+    int id;
+    const char *name;
+    int power;
+    int accuracy;
+    int maxPP;
+    AttackCategory category;
+    PokemonType type;
+} AttackData;
+
+typedef struct {
+    int id;
+    const char *name;
+    int baseHp;
+    int baseAttack;
+    int baseSpAttack;
+    int baseDefense;
+    int baseSpDefense;
+    int baseSpeed;
+    PokemonType type1;
+    PokemonType type2; // same as type1 if single type
+    const AttackData *moves[4];
+} PokemonData;
+
+typedef struct {
+    const PokemonData* data; // points back to the species template, no copying needed
+    int frontFrame_ID;
+    int backFrame_ID;
+} pokemonID;
+
+typedef struct {
+    pokemonID id;
+    int level;
+    int exp;
+    bool alive;
+    int maxHp;
+    int scaledStatsWithLevel[6]; //{hp, attack, sp attack, def, sp def, speed}
+    int currentPP[4]; // current pp for each move
+    bool inBattle;
+    StatusCondition status;
+    int sleepTurnsRemaining;
+    PokemonType type1;
+    PokemonType type2; // same as type1 if single type
+    const AttackData* attacks[4]; // 4 attacks per pokemon
+} pokemonInBattle;
+
+// type effectiveness table [attacking type][defending type]
+static const float typeChart[NUM_TYPES][NUM_TYPES] = {
+//      NOR  FIR  WAT  ELE  GRA  ICE  FIG  POI  GRO  FLY  PSY  BUG  ROC  GHO  DRA  DAR  STE  FAI
+       {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.0, 1.0, 1.0, 0.5, 1.0}, // normal
+       {1.0, 0.5, 0.5, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 0.5, 1.0, 2.0, 1.0}, // fire
+       {1.0, 2.0, 0.5, 1.0, 0.5, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 2.0, 1.0, 0.5, 1.0, 1.0, 1.0}, // water
+       {1.0, 1.0, 2.0, 0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 2.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0}, // electric
+       {1.0, 0.5, 2.0, 1.0, 0.5, 1.0, 1.0, 0.5, 2.0, 0.5, 1.0, 0.5, 2.0, 1.0, 0.5, 1.0, 0.5, 1.0}, // grass
+       {1.0, 0.5, 0.5, 1.0, 2.0, 0.5, 1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 0.5, 1.0}, // ice
+       {2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 0.5, 1.0, 0.5, 0.5, 0.5, 2.0, 0.0, 1.0, 2.0, 2.0, 0.5}, // fighting
+       {1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 0.5, 0.5, 1.0, 1.0, 1.0, 0.5, 0.5, 1.0, 1.0, 0.0, 2.0}, // poison
+       {1.0, 2.0, 1.0, 2.0, 0.5, 1.0, 1.0, 2.0, 1.0, 0.0, 1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 2.0, 1.0}, // ground
+       {1.0, 1.0, 1.0, 0.5, 2.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 1.0, 1.0, 0.5, 1.0}, // flying
+       {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 0.0, 0.5, 1.0}, // psychic
+       {1.0, 0.5, 1.0, 1.0, 2.0, 1.0, 0.5, 0.5, 1.0, 0.5, 2.0, 1.0, 1.0, 0.5, 1.0, 2.0, 0.5, 0.5}, // bug
+       {1.0, 2.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 0.5, 2.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0}, // rock
+       {0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 2.0, 1.0, 0.5, 1.0, 1.0}, // ghost
+       {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 0.5, 0.0}, // dragon
+       {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0, 0.5, 1.0, 0.5, 1.0, 0.5}, // dark
+       {1.0, 0.5, 0.5, 0.5, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 0.5, 2.0}, // steel
+       {1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 0.5, 0.5}, // fairy
+};
+
+static const AttackData TACKLE = {
+    .id = 1,
+    .name = "Tackle",
+    .power = 40,
+    .accuracy = 100,
+    .maxPP = 35,
+    .category = ATTACK_PHYSICAL,
+    .type = TYPE_NORMAL
+};
+
+static const PokemonData CHARIZARD = {
+    .id = 1,
+    .name = "Charizard",
+    .baseHp = 78,
+    .baseAttack = 84,
+    .baseSpAttack = 109,
+    .baseDefense = 78,
+    .baseSpDefense = 85,
+    .baseSpeed = 100,
+    .type1 = TYPE_FIRE,
+    .type2 = TYPE_FLYING,
+    .moves = {
+        &TACKLE,
+        NULL,
+        NULL,
+        NULL
+    }
+};
+
+// init
+void initPokemon(PokemonData* pokemon, int id, const char* name,
+                 int baseHp, int baseAttack, int baseSpAttack,
+                 int baseDefense, int baseSpDefense, int baseSpeed,
+                 PokemonType type1, PokemonType type2,
+                 const AttackData* move1, const AttackData* move2,
+                 const AttackData* move3, const AttackData* move4);
+void initPokemonInBattle(pokemonInBattle* pokemon, const PokemonData* template, int level);
+
+// stats
+void scaleStatsWithLevel(pokemonInBattle* pokemon);
+void levelUp(pokemonInBattle* pokemon);
+void gainExp(pokemonInBattle* pokemon, pokemonInBattle* defeatedPokemon);
+int expRequiredAtLevel(int level);
+int experienceGained(int levelSelf, int levelOpponent);
+
+// type
+float getTypeEffectiveness(PokemonType attackType, PokemonType defendType1, PokemonType defendType2);
+
+// battle actions
+void dealDamage(pokemonInBattle* attacker, pokemonInBattle* target, int baseDamage, int damageType, PokemonType moveType);
+void takeDamage(pokemonInBattle* target, int incomingDamage, int damageType);
+bool useAttack(pokemonInBattle* attacker, pokemonInBattle* target, int attackIndex);
+int determineturnOrder(pokemonInBattle* pokemon1, pokemonInBattle* pokemon2);
+bool attemptFlee(pokemonInBattle* fleeing, pokemonInBattle* opponent);
+
+// status
+void applyStatusEffect(pokemonInBattle* pokemon, StatusCondition status);
+void tickStatusEffect(pokemonInBattle* pokemon);
+bool canAct(pokemonInBattle* pokemon);
+
+// healing
+void healPokemon(pokemonInBattle* pokemon, int amount);
+void restorePP(pokemonInBattle* pokemon, int attackIndex);
+void restoreAllPP(pokemonInBattle* pokemon);
+void fullHeal(pokemonInBattle* pokemon);
+
+// catch
+bool attemptCatch(pokemonInBattle* wildPokemon, float ballModifier);
+
+// getters
+bool isAlive(pokemonInBattle* pokemon);
+int getHp(pokemonInBattle* pokemon);
+int getAttack(pokemonInBattle* pokemon);
+int getSpAttack(pokemonInBattle* pokemon);
+int getDef(pokemonInBattle* pokemon);
+int getSpDef(pokemonInBattle* pokemon);
+int getSpd(pokemonInBattle* pokemon);
