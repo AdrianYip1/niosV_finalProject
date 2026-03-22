6f113d550dc1406f0138f478c62edfa9d0c32fa0
Adrian Yip <yipadri1@WIN.ecf.utoronto.ca>
2026-03-22 18:23:04 -0400

    commented out textbox

--- software/main.c
+++ software/main.c
@@ -550,7 +550,7 @@ int main(void)
         draw_sprite_any(charmanderBox, CHARIZARD_BOX_WIDTH, CHARIZARD_BOX_HEIGHT, PARTY_5_X, PARTY_5_Y, TRANSPARENT_COLOUR);
         draw_sprite_any(charmeleonBox, CHARIZARD_BOX_WIDTH, CHARIZARD_BOX_HEIGHT, PARTY_6_X, PARTY_6_Y, TRANSPARENT_COLOUR);
 
-        if (dialogActive) {
+ /*       if (dialogActive) {
             gameplayTextboxDone = draw_textbox_animated_text(
                 textBoxSprite, TEXTBOX_X, TEXTBOX_Y, TEXT_MESSAGES[gameplayTextboxIndex], BLACK);
 
@@ -569,6 +569,8 @@ int main(void)
                 }
             }
         }
+
+        */
 
         // Arrow cursor drawn last (on top of everything, including textbox).
         {
