a826b081fc0cbc3bb1f15a5b55852f3618bcb87f
Adrian Yip <yipadri1@WIN.ecf.utoronto.ca>
2026-03-28 14:50:24 -0400

    a

--- software/main.c
+++ software/main.c
@@ -1134,7 +1134,7 @@ int main(void)
                     if (isSelected) {
                         draw_sprite_any(partyLeaderSelectedSprite,
                                         MENU_PARTY_LEADER_SELECTED_WIDTH, MENU_PARTY_LEADER_SELECTED_HEIGHT,
-                                        slotX + leaderSelectedDx - 3, slotY + leaderSelectedDy + colYOffset - 6,
+                                        slotX + leaderSelectedDx - 4, slotY + leaderSelectedDy + colYOffset - 4,
                                         TRANSPARENT_COLOUR);
                     } else {
                         draw_sprite_any(partyLeaderUnselectedSprite,
