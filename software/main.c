83a0bd4aea7c4d4b2a4f3d95c61a2f8c26a00000
Adrian Yip <yipadri1@WIN.ecf.utoronto.ca>
2026-03-29 15:56:34 -0400

    pixel locations

--- software/main.c
+++ software/main.c
@@ -154,9 +154,9 @@
 #define PC_MENU_PARTY_Y0 (PC_MENU_PARTY_BOX_Y + 10) - 2
 #define PC_MENU_PARTY_STEP_X 53 - 3
 #define PC_MENU_PARTY_STEP_Y 50 - 10 - 5 - 5
-// Some party-box layouts have the right column vertically offset vs the left.
-// Positive moves the right column down.
-#define PC_MENU_PARTY_RIGHT_COL_DY (25)
+
+
+#define PC_MENU_PARTY_RIGHT_COL_DY (25) //down 25
 
 
 //location for drawing names, hp, level
@@ -3895,7 +3895,7 @@ int main(void)
             // Base UI
             draw_sprite_any(pcBoxBackgroundSprite,
                             PC_MENU_PC_BOX_BACKGROUND_WIDTH, PC_MENU_PC_BOX_BACKGROUND_HEIGHT,
-                            0, (120 - (PC_MENU_PARTY_BOX_HEIGHT / 2)) - 35 + 10 - 5,
+                            0, (120 - (PC_MENU_PARTY_BOX_HEIGHT / 2)) - 35 + 10 - 5 + 5,
                             TRANSPARENT_COLOUR);
 
             draw_sprite_any_rot90_cw(pcBoxBlueSprite,
@@ -3905,17 +3905,17 @@ int main(void)
 
             draw_sprite_any(leftArrowSprite,
                             PC_MENU_LEFT_ARROW_WIDTH, PC_MENU_LEFT_ARROW_HEIGHT,
-                            PC_MENU_LEFT_ARROW_X, PC_MENU_ARROW_Y,
+                            PC_MENU_LEFT_ARROW_X, PC_MENU_ARROW_Y + 7,
                             TRANSPARENT_COLOUR);
 
             draw_sprite_any(rightArrowSprite,
                             PC_MENU_RIGHT_ARROW_WIDTH, PC_MENU_RIGHT_ARROW_HEIGHT,
-                            PC_MENU_RIGHT_ARROW_X, PC_MENU_ARROW_Y,
+                            PC_MENU_RIGHT_ARROW_X, PC_MENU_ARROW_Y + 7,
                             TRANSPARENT_COLOUR);
 
             draw_sprite_any(pcLabelSprite,
                             PC_MENU_PC_LABEL_WIDTH, PC_MENU_PC_LABEL_HEIGHT,
-                            20, 20 + 3,
+                            20 + 3, 20 + 3,
                             TRANSPARENT_COLOUR);
 
             draw_sprite_any(partyBoxSprite,
@@ -4020,7 +4020,7 @@ int main(void)
                 if (pokeSprite != NULL) {
                     draw_sprite_any(pokeSprite,
                                     MENU_POKEMON_SPRITE_WIDTH, MENU_POKEMON_SPRITE_HEIGHT,
-                                    x, y,
+                                    x - 10, y + 20,
                                     TRANSPARENT_COLOUR);
                 }
             }
@@ -4035,7 +4035,7 @@ int main(void)
                 if (pokeSprite != NULL) {
                     draw_sprite_any(pokeSprite,
                                     MENU_POKEMON_SPRITE_WIDTH, MENU_POKEMON_SPRITE_HEIGHT,
-                                    x, y,
+                                    x - 10, y + 20,
                                     TRANSPARENT_COLOUR);
                 }
             }
